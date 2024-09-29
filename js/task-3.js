5faf64e24aef483f4a75a28cceeb8cf2c476ec0d
Ferens Anastasiia <nastyaferens2.0@gmail.com>
2024-09-30 00:05:55 +0300

    update js

---
+++ js/task-3.js
@@ -0,0 +1,8 @@
+function getElementWidth(content, padding, border) {
+    return Number.parseFloat(content) +
+          (Number.parseFloat(padding) * 2) +
+          (Number.parseFloat(border) * 2);
+}
+getElementWidth("50px", "8px", "4px");
+getElementWidth("60px", "12px", "8.5px");
+getElementWidth("200px", "0px", "0px");

42afab129dc7c3a6371a763b5b99f9effcaaab3a
Ferens Anastasiia <nastyaferens2.0@gmail.com>
2024-10-02 23:22:11 +0300

    update

--- js/task-2.js
+++ js/task-2.js
@@ -3,5 +3,5 @@ function getShippingMessage(country, price, deliveryFee) {
     return `Shipping to ${country} will cost ${totalPrice} credits`
 }
 console.log(getShippingMessage("Australia", 120, 50));
-conlsole.log(getShippingMessage("Germany", 80, 20));
+console.log(getShippingMessage("Germany", 80, 20));
 console.log(getShippingMessage("Sweden", 100, 20));
